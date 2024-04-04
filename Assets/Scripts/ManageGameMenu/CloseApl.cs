31bf11f9ea830b22f258136971cf7fc9e15833db
Sgifch <mivachi@inbox.ru>
2024-04-04 21:01:38 +0400

    Добавила кнопку выхода

---
+++ Assets/Scripts/ManageGameMenu/CloseApl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseApl : MonoBehaviour
+{
+    public void ClickClose()
+    {
+        Application.Quit();
+    }
+}
