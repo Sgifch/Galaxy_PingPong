ede8e8fd5a31bce00b45a8c92a391ed9c60bba6b
Sgifch <mivachi@inbox.ru>
2024-05-30 21:53:20 +0400

    scuf

--- Assets/Script/Gate1Script.cs
+++ Assets/Script/Gate1Script.cs
@@ -6,16 +6,13 @@ using Mirror;
 public class Gate1Script : NetworkBehaviour
 {
     public GameManager gameManager;
-    public Animator anim;
 
     void OnTriggerEnter2D (Collider2D collider)
     {
         if (collider.gameObject.tag == "Ball")
         {
             gameManager.CmdDestroyObject(collider.gameObject, 1);
-            anim.SetBool("TriggerEnt", true);
-            print("yes");
-            //anim.SetBool("TriggerEnt", false);
+            
         }
     }
 
